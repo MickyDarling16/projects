38d2dbf30a4074f668916e916d9a74ffff11cd5c
Michael Adu (XPS 15) <michaelybadu@gmail.com>
2025-08-03 00:53:51 -0400

    Fix missing newline at end of truncate.sh script

--- Coursera/course7/truncate.sh
+++ Coursera/course7/truncate.sh
@@ -4,4 +4,4 @@ DATABASE=sakila
 
 mysql -Nse 'show tables' sakila | \
     while read table; do mysql \
-    -e "use sakila;SET FOREIGN_KEY_CHECKS=0;truncate table $table;SET FOREIGN_KEY_CHECKS=1;" ;done
+    -e "use sakila;SET FOREIGN_KEY_CHECKS=0;truncate table $table;SET FOREIGN_KEY_CHECKS=1;";done
